209a9dd16c2326f8ecea9475be90eacaf6d3b015
dasha <dasha.kurushina@gmail.com>
2018-03-03 17:16:59 +0500

    Реализован разбор параметров командной строки lab_5 rand 4 5 создает двумерный массив, размерностью 4 на 5 , заполненный случайными числами. lab_5 rand 4 создает одномерный массив, размерностью 4, заполненный случайными числами. lab_5 1 2 3 4 5 создает однометрый массив с числами 1 2 3 4 5.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args[0] == "rand")
+		{
+			if(args.length == 2)
+			{
+				//массив 1
+			}
+			else
+			{
+			//массив 2	
+			}
+		}
+		else
+		{
+			//массив 1
+		}
+
+	}
+
+}
