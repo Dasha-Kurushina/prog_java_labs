0110738a1255b2dd4e23659effa876b63af5a554
dasha <dasha.kurushina@gmail.com>
2018-03-03 18:37:40 +0500

    реализован конструктор двумерного массива

--- src/D2_Random_Array.java
+++ src/D2_Random_Array.java
@@ -2,8 +2,18 @@
 
 public class D2_Random_Array extends My_Array {
 
-	public D2_Random_Array(int i, int j) {
-		// TODO Auto-generated constructor stub
+	private int[][] data;
+
+	public D2_Random_Array(int n, int m) 
+	{
+		data = new int[n][m];
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < m; j++)
+			{
+				data[i][j] = 0;
+			}
+		}
 	}
 
 }
